df972b1960ded05741b1eada46d880c98a157a4d
fazlerabbi125 <fazle.rabbi125faiyaz@gmail.com>
2025-08-17 00:43:14 +0600

    fix: Fix cor_origns settings and user role column

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from settings import config
-from db import init_db
 
 api = FastAPI() # Create the FastAPI application instance
 api.add_middleware(
